da171f9f759d471923d4299d6981aac15027108b
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-11-01 14:51:15 +0300

    Добавлен новый маршрут

--- src/Plugins/routes/scoreTableRoutes.ts
+++ src/Plugins/routes/scoreTableRoutes.ts
@@ -1,5 +1,5 @@
 import { FastifyPluginAsync } from "fastify";
-import { getScores, addScore, getScoreById } from "../../db/scoreService";
+import { getScores, addScore, getScoreById, getScoreByName } from "../../db/scoreService";
 import fp from "fastify-plugin";
 
 const plugin: FastifyPluginAsync = async (server) => {
@@ -18,6 +18,21 @@ const plugin: FastifyPluginAsync = async (server) => {
     const { id } = request.params as { id: string };
     try {
       const user = await getScoreById(server, Number(id));
+      if (user) {
+        reply.send(user);
+      } else {
+        reply.status(404).send({ error: 'User not found' });
+      }
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send({ error: 'Internal Server Error' });
+    }
+  });
+
+  server.get('/score/name/:player_name', { preHandler: [server.authenticate] }, async (request, reply) => {
+    const { player_name } = request.params as { player_name: string };
+    try {
+      const user = await getScoreByName(server, player_name);
       if (user) {
         reply.send(user);
       } else {
--- src/db/scoreService.ts
+++ src/db/scoreService.ts
@@ -9,6 +9,10 @@ export async function getScoreById(app: FastifyInstance, id: number) {
   const [rows] = await app.mysql.query('SELECT * FROM Score_Table WHERE id = ?', [id]);
   return rows
 }
+export async function getScoreByName(app: FastifyInstance, player_name: string) {
+  const [rows] = await app.mysql.query('SELECT * FROM Score_Table WHERE player_name = ?', [player_name]);
+  return rows
+}
 
 export async function addScore(app: FastifyInstance, name: string, score: number) {
   const [result] : any = await app.mysql.query('INSERT INTO Score_Table (player_name, score) VALUES (?, ?) ON DUPLICATE KEY UPDATE score = ?', [name, score, score]);
