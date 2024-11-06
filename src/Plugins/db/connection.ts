2967ecf70bb60e36cfa74d38fce74dc581efc527
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-11-06 13:58:22 +0300

    Update connection.ts
    Переход на PoolConnection

--- src/Plugins/db/connection.ts
+++ src/Plugins/db/connection.ts
@@ -5,11 +5,15 @@ import fp from "fastify-plugin";
 
 const plugin: FastifyPluginAsync = async (server) => {
 
-    const connectionService = await mysql.createConnection({
+    const connectionService = await mysql.createPool({
         host: '5.34.214.49',
         user: 'PLAYER_USER',
         password: 'wWvIaVrx@Q7~',
         database: 'HarvestingBase',
+        waitForConnections: true,
+        connectionLimit: 20,  // max connections
+        queueLimit: 0,
+        idleTimeout: 60000,   // idle timeout for connections
       });
       
     server.decorate('mysql', connectionService);
@@ -17,7 +21,7 @@ const plugin: FastifyPluginAsync = async (server) => {
 
 declare module 'fastify' {
     interface FastifyInstance {
-        mysql: mysql.Connection;
+        mysql: mysql.Pool;
     }
 }
 
