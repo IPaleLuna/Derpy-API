aa5f0cca11b5168986d0591fde2e0b5610b316ff
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-11-06 13:58:34 +0300

    add CORS
    Настройка CORS

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,20 @@ import app from "./app";
 import authPlugin from "./Plugins/auth/authPlugin";
 import { connectionPlugin } from "./Plugins/db/connection";
 import { scoreTableRoutesPlugin } from "./Plugins/routes/scoreTableRoutes";
+import cors from '@fastify/cors';
 
 const start = async () => {
   try {
     await app.register(connectionPlugin)
     await app.register(authPlugin);
     await app.register(scoreTableRoutesPlugin, { prefix: '/score' })
+
+    app.register(cors, {
+      origin: '*', // разрешить все домены (измените при необходимости)
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // методы, которые разрешены
+      allowedHeaders: ['authorization', 'Content-Type', 'Accept-Encoding','Accept-Charset'], // разрешённые заголовки
+      credentials: true // если требуется поддержка сессий или cookie
+    });
 
     await app.listen({ host: '5.34.214.49', port: 3000 });
     
